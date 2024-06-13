842536065217eb9e205380ac351ae7f71204829b
haendle <113534244+haendle@users.noreply.github.com>
2024-06-13 15:33:04 +0300

    Add files via upload

---
+++ RDP-NET/RdpClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+using RdpClient.Forms;
+
+namespace RdpClient
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            StartDialog startDialog = new StartDialog();
+            startDialog.ShowDialog();
+
+            if (startDialog.SwitchWnd && startDialog.IsLocal)
+            {
+                ViewerDialog viewerDialog = new ViewerDialog
+                (
+                    startDialog.ConnectionString,
+                    startDialog.HostnameLAN,
+                    startDialog.AccessLevel,
+                    startDialog.PasswordLAN,
+                    startDialog.UsernameLAN,
+                    startDialog.IsLocal
+                );
+
+                startDialog?.Dispose();
+
+                viewerDialog.ShowDialog();
+            }
+
+            else if (startDialog.SwitchWnd && (!startDialog.IsLocal))
+            {
+                ViewerDialog viewerDialog = new ViewerDialog
+                (
+                    startDialog.ConnectionString,
+                    startDialog.HostnameWAN,
+                    startDialog.AccessLevel,
+                    startDialog.PasswordWAN,
+                    startDialog.UsernameWAN,
+                    startDialog.IsLocal
+                );
+
+                startDialog?.Dispose();
+
+                viewerDialog.ShowDialog();
+            }
+        }
+    }
+}
